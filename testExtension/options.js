f503080fd28e9e5852f4719ef091d5a067f71c38
Ryan Diaz <ryandiaz1090@gmail.com>
2020-02-24 18:18:29 -0500

    Added basic functionality to search for data on mixer streams

--- testExtension/options.js
+++ testExtension/options.js
@@ -4,16 +4,30 @@ const BASE_URL_TWITCH = "https://api.twitch.tv/helix";
 const CLIENT_ID_TWITCH = "wn4jubf3xbpbk49l089pb1p429qlce";
 
 //An example API call to mixer getting specific channel information
-const TEST_URL = "https://mixer.com/api/v1/channels/misterjoker";
+const BASE_URL_MIXER = "https://mixer.com/api/v1/channels/";
 
+//Get the button to add streamer, and run addStreamer() on click
+const button = document.querySelector('button');
+button.addEventListener('click', getStreamer);
 
-async function getData() {
+//Function to get streamer data from Mixer's API
+function getStreamer() {
+	const user = document.querySelector("#streamId").value;
+	console.log(user);
 
-	const response = await fetch(TEST_URL);
+	//Need to deal with promise
+	getData(user);
+}
+
+//Get data from Mixer API based on streamer user name
+async function getData(userId) {
+
+	const response = await fetch(BASE_URL_MIXER + userId);
 	//Convert response into json
 	const data = await response.json();
 	//Print data to console
 	console.log(data);
 }
 
-getData();
+//Commented out to avoid constantly calling API during testing
+//getData();
