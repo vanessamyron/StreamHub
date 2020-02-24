86fa28ea00267a0e238fff053aa3d3967102d89c
Ryan Diaz <ryandiaz1090@gmail.com>
2020-02-24 16:41:03 -0500

    Added a simple test API call to the options.html to display data in the console.
    Added our twitch api key in the options.js file.

--- testExtension/options.js
+++ testExtension/options.js
@@ -1,15 +1,18 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-		for (let item of kButtonColors) {
-				let button = document.createElement('button');
-				button.style.backgroundColor = item;
-				button.addEventListener('click', function() {
-						chrome.storage.sync.set({color: item}, function() {
-								console.log('color is ' + item);
-						})
-				});
-				page.appendChild(button);
-		}
+const BASE_URL_TWITCH = "https://api.twitch.tv/helix";
+
+//Our API Key/client id for twitch.tv
+const CLIENT_ID_TWITCH = "wn4jubf3xbpbk49l089pb1p429qlce";
+
+const TEST_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+
+async function getData() {
+
+	const response = await fetch(TEST_URL);
+	//Convert response into json
+	const data = await response.json();
+	//Print data to console
+	console.log(data);
 }
-constructOptions(kButtonColors);
+
+getData();
