a5bfbfb2621f02e5c1bfd66ef04b8059a0cb178f
IsaacWolfe <iwwolfe97@gmail.com>
2020-03-10 19:07:47 -0400

    Added all the webpage files from public repo

---
+++ webpage/js/home.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+		$("#add-streamer").click(function() {
+				$(this).attr({
+						"id": "homepage"
+						"src": "./photos/home.png",
+						"alt": "Homepage button"
+				});
+				$("input").attr("placeholder","Type in Streamers name to search and add them");
+				$(".streamers").remove();
+		});
+		$("#homepage").click(function() {
+				$(this).attr({
+						"id": "add-streamer"
+						"src": "./photos/plus.png",
+						"alt": "Add streamers button"
+				});
+		});
+});
