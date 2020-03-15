56e89db0dc46e21f3861c9ef31474ad607a31956
Ryan Diaz <ryandiaz1090@gmail.com>
2020-03-15 05:07:44 -0400

    f

---
+++ Streamsters/background.js
@@ -0,0 +1,16 @@
+/*function addToStorage(user, callback) {
+    console.log("Adding user: " + user);
+    callback();
+}
+
+function addStreamer(status, name, viewers){
+	let tableRef = document.getElementById("onlineStreamersTable");
+	let row = tableRef.insertRow(1);
+	let cell1 = row.insertCell(0);
+	let cell2 = row.insertCell(1);
+	let cell3 = row.insertCell(2);
+	cell1.innerHTML = status;
+	cell2.innerHTML = name;
+	cell3.innerHTML = viewers;
+}
+*/
