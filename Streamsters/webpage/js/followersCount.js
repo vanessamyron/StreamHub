530b3a08ad2f7c586bd5ee682d2549a5760c8410
vanessamyron <vanessa.myron@yahoo.co.uk>
2020-04-03 17:47:37 -0400

    Followers Count
    In Progress -> To get the total number of followers

---
+++ Streamsters/webpage/js/followersCount.js
@@ -0,0 +1,216 @@
+// $(document).ready(function() {
+// 		$("#search").focus(function(){ //To test if streamers has element of active streamers when searching
+// 				var full = $("#streamers").has("img").length ? true : false;
+// 				if (full == false){
+// 						$("#streamers").empty();
+// 				}
+// 		});
+// 		var getName = $("#search").val();
+// 		if (getName == ’){
+// 				$("#streamers").html("<h2>Please enter text to search.</h2>");
+// 		} else {
+// 				$("#streamers").html("<h2>Loading streamers</h2>");
+// 		}
+// });
+const BASE_URL_TWITCH = "https://api.twitch.tv/helix/streams?user_login=";
+
+//Our API Key/client id for twitch.tv
+const CLIENT_ID_TWITCH = "wn4jubf3xbpbk49l089pb1p429qlce";
+
+//An example API call to mixer getting specific channel information
+const BASE_URL_MIXER = "https://mixer.com/api/v1/channels/";
+
+// An API to get user follow
+const GET_URL_FOLLOW = "https://api.twitch.tv/kraken/users/<user ID>/follows/channels";
+
+//Get the button to add streamer, and run streamSelected() on click
+const addStreamButton = document.getElementById("addStreamButton");
+addStreamButton.addEventListener('click', streamSelected);
+
+function streamSelected() {
+	let ele = document.getElementsByName('website');
+
+	for(i = 0; i < ele.length; i++) {
+		if(ele[i].checked) {
+			switch(ele[i].value) {
+				case "twitch":
+					getStreamerTwitch();
+					return;
+				case "mixer":
+					getStreamerMixer();
+					return;
+			}
+		}
+	}
+
+}
+
+//Get the homepage button, and run streamSelected() on click
+const button = document.getElementById("homePageButton");
+button.addEventListener('click', goHome);
+
+function goHome() {
+	window.open("webpage/index.html", "_blank");
+}
+
+//Function to get streamer data from Twitch's API
+/* Twitch seems to require that the client-id be in the Javascript Header Object, more info on those:
+   https://developer.mozilla.org/en-US/docs/Web/API/Headers
+   https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch */
+async function getStreamerTwitch() {
+	const user = document.querySelector("#streamId").value;
+	console.log("Calling twitch api for user: " + user);
+
+	fetch(BASE_URL_TWITCH + user, {
+		method: 'GET', // or 'PUT'
+		headers: {
+			'Content-Type': 'application/json',
+			'Client-ID': CLIENT_ID_TWITCH,
+		},
+	})
+	.then((response) => response.json())
+	.then((user) =>	console.log('Success:', user));
+
+		//Placing JSON array object into obj for better readability later
+		const obj = user.data[0];
+		if(obj === undefined) {
+
+
+			addStreamer("Offline" , document.querySelector("#streamId").value , " " , " ", " ");
+		}
+		else {
+			addStreamer(obj.type, obj.user_name, obj.title ,obj.viewer_count, obj.follows);
+			/*
+			WOrking on later -
+			chrome.runtime.getBackgroundPage(function(backgroundPage) {
+				backgroundPage.addToStorage(user,addStreamer(obj.type, obj.user_name, obj.viewer_count));
+			});
+			*/
+		}
+	
+}
+
+//Function to get streamer data from Mixer's API
+async function getStreamerMixer() {
+	const user = document.querySelector("#streamId").value;
+	console.log("Calling mixer api for user: " + user);
+
+	fetch(BASE_URL_MIXER + user, {
+		method: 'GET', // or 'PUT'
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((response) => response.json())
+		.then((user) => {
+			console.log('Success:', user);
+
+			//User is already a parsed JSON object, can access data directly and check if user.online === true
+			if(user.online === true) {
+				let followers = user.stream.channel.follows;
+				addStreamer(user.online, user.token, user.title ,user.viewersCurrent, followers);
+				console.log(user.online);
+				console.log(user.viewersCurrent);
+				console.log(followers);
+			}
+			else
+				console.log("Offline");
+		})
+}
+
+function addStreamer(status, name, title, viewers, followers){
+	let tableRef = document.getElementById("onlineStreamersTable");
+	let row = tableRef.insertRow(1);
+	let cell1 = row.insertCell(0);
+	let cell2 = row.insertCell(1);
+	let cell3 = row.insertCell(2);
+	let cell4 = row.insertCell(3);
+	let cell5 = row.insertCell(4);
+
+
+	if(status == "Offline"){
+
+	var tds = document.getElementsByTagName("td");
+	tds[0].style.color = "#FF0000";
+
+
+	cell1.innerHTML = status;
+	cell2.innerHTML = name;
+	cell3.innerHTML = title;
+	cell4.innerHTML = viewers;
+	cell5.innerHTML = followers;
+	}else{
+
+	var tds = document.getElementsByTagName("td");
+	tds[0].style.color = "#008000";
+
+	cell1.innerHTML = status;
+	cell2.innerHTML = name;
+	cell3.innerHTML = title;
+	cell4.innerHTML = viewers;
+	cell5.innerHTML = followers;
+	}
+}
+
+function getFollowers(name){
+return new Promise((resolve,reject) => {
+	request({GET_URL_FOLLOW, json: true }, (err,resp,body) => {
+		if(err == true){
+			reject(err)
+	}
+
+		const PEOPLE_FOLLOWERS = body.follows.map(follower => {
+			return { id: follower.user.id, name: follower.user.name }
+		})
+				resolve(PEOPLE_FOLLOWERS)
+	})
+})
+
+}
+
+var followersCount = [];
+
+$.ajax({
+  dataType: "jsonp",
+  url: 'https://api.twitch.tv/kraken/users/hicough/follows/channels',
+  }).done(function ( data ) {
+  var total = data._total - 1;
+  var follows = data.follows;
+  for (var start = 0; start < total; start++) {
+    var userInfo = follows[start].channel;
+    var addHtml = "<div class='col-xs-12 row'><a href='" + userInfo.url + "'>";
+    addHtml += "<div class='col-xs-2'><img class='img-responsive' src='" + userInfo.logo + "'></div>";
+    addHtml += "<div class='col-xs-10'><h4>" + userInfo.display_name + "<div class=" + userInfo.name + "></div>";
+    addHtml += "<small>" + userInfo.status + "</small></h4></div>";
+    addHtml += "</a></div>";
+    $(".appendData").append(addHtml);
+    
+    followersCount.push(userInfo.name);
+    
+    if (i === (total-1)) {
+     getLive();
+    }
+  }
+});
+
+function getLive() {
+  for (var i = 0; i < followersCount.length; i++) {
+    var channelUrl = "https://api.twitch.tv/kraken/streams/" + followersCount[i];
+    $.ajax({
+      dataType: "jsonp",
+      url: channelUrl,
+      async: false,
+    }).done(function ( newObject) {
+      if (newObject.stream) {
+        
+        var getLive = "<a href=" + newObj.stream.channel.url + "><div class='col-xs-4'>";
+        getLive+= "<img src=" + newObj.stream.preview.medium + " class='img-responsive'>";
+        getLive += "<h4>" + newObj.stream.channel.display_name + "<br><small>playing " + newObj.stream.channel.game;
+        getLive += "</a></small></h4></div>";
+        
+        $(".viewLive").append(getLive);
+      }
+    });  
+  }
+  
+}
