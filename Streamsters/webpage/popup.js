87826b4dbb6b52ca6a6ec824c0a8927f036ade4c
IsaacWolfe <iwwolfe97@gmail.com>
2020-04-24 19:27:55 -0400

    Removed unnecessary add button

--- Streamsters/webpage/popup.js
+++ Streamsters/webpage/popup.js
@@ -159,13 +159,9 @@ function showData(){
 		let cell2 = row.insertCell(1);
 		let cell3 = row.insertCell(2);
 		let cell4 = row.insertCell(3);
-		let cell5 = row.insertCell(4);
 
 
 		var tds = document.getElementsByTagName("td");
-		let btn = document.createElement("button");
-		btn.innerHTML = "<img src='./photos/plus.png' alt='Add streamers button.'>";
-		cell5.appendChild(btn);
 
 	if(arr[i].status1 == "live"){
 		tds[0].style.color = "#008000";
